27b853819b6f878a7e0aa962ab572c373df9e496
chadtied <96424234+chadtied@users.noreply.github.com>
2024-05-06 13:45:23 +0800

    Update stock_crowl.py

--- stock_crowl.py
+++ stock_crowl.py
@@ -4,11 +4,15 @@ import requests
 
 #建立變數
 driver= Driver(uc= True, incognito= False, headless= True)
+download_url= ''
 
 driver.get('https://data.gov.tw/dataset/11549')
-download_url= driver.find_element(By.CSS_SELECTOR,"#__nuxt > div > div > main > div.page > div.table.table--fixed.od-table.od-table--bordered.print-table > div:nth-child(2) > div:nth-child(2) > ul > li > a").get_attribute('href')
-response= requests.get(download_url)
+web_elements= driver.find_elements(By.CSS_SELECTOR,"ul > li.resource-item > a")
 
+for element in web_elements:
+    if "CSV" in element.text:   download_url= element.get_attribute('href')
+
+response= requests.get(download_url)
 if response.status_code == 200:
     with open('./stock.csv', 'w') as f: #下載路徑依照需求更改，目前為預設路徑
         f.write(response.text)
